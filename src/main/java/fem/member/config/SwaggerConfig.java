04766b2087854e189f84e5c95b2deccf106213d7
이상훈 <slee@i-magicsoft.com>
2024-01-31 16:28:23 +0900

    swagger 설정

---
+++ src/main/java/fem/member/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package fem.member.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("회원 API")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
