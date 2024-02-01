ad77ef0250f91da733cd6754530afcf78c39d187
이상훈 <slee@i-magicsoft.com>
2024-02-01 13:52:26 +0900

    usecase 추가

--- src/main/java/fem/member/application/usecase/SavePointUsecase.java
+++
@@ -1,8 +0,0 @@
-package fem.member.application.usecase;
-
-import fem.member.domain.model.vo.IDName;
-import fem.member.framework.web.dto.MemberOutputDTO;
-
-public interface SavePointUsecase {
-    MemberOutputDTO savePoint(IDName idName, long point);
-}
--- src/main/java/fem/member/application/usecase/UsePointUsecase.java
+++
@@ -1,8 +0,0 @@
-package fem.member.application.usecase;
-
-import fem.member.domain.model.vo.IDName;
-import fem.member.framework.web.dto.MemberOutputDTO;
-
-public interface UsePointUsecase {
-    MemberOutputDTO usePoint(IDName idName, long point);
-}
