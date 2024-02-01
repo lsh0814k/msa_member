76700dfcd25aa20fb98a54260985e2ba6388f707
이상훈 <slee@i-magicsoft.com>
2024-02-01 13:41:50 +0900

    도메인, VO 생성

---
+++ src/main/java/fem/member/domain/model/vo/CreatedTime.java
@@ -0,0 +1,20 @@
+package fem.member.domain.model.vo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class CreatedTime {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+}
